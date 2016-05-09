116873ff12b212ed6df621d36738747297f53dd0
klklmoon <leiwei8509@126.com>
2016-05-10 00:11:53 +0800

    111

---
+++ lib/actions/ajaxs/ajaxAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Su qian <aoxue.1988.su.qian@163.com> 2010-11-2
+ * @link http://show.pipi.cn
+ * @copyright Copyright &copy; 2003-2010 show.pipi.cn
+ * @license
+ */
+
+
+class ajaxAction extends PipiAction{
+	
+	public function run(){
+		echo 'ajax action is running';
+	}
+}
